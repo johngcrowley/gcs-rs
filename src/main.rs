24db9cd9ef009d9cb84fe210e4a045bb0de148bc
john <johncrowley547@gmail.com>
2025-01-25 22:07:52 -0600

    Returning resumable upload URI.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 #![allow(dead_code)]
 #![allow(unused)]
 
-use anyhow::Result;
+use anyhow::{Error, Result};
+use gcp_auth::Token;
 use http::Method;
+use reqwest::{header, Client};
+use std::sync::Arc;
 
 pub mod cli {
 
@@ -11,54 +14,119 @@ pub mod cli {
     pub fn parse_args() -> ArgMatches {
         let matches = Command::new("gcs-rs")
             .about("A rust-based, barebones GCS client based on Google Cloud HTTP API")
-            .arg(arg!(--op <VALUE>).required(true))
+            .arg(arg!(--op <VALUE>))
             .arg(arg!(--uri <VALUE>))
             .get_matches();
 
-        println!("Value for uri: {:?}", matches.get_one::<String>("uri"));
-
-        println!(
-            "Value for op: {:?}",
-            matches.get_one::<String>("op").expect("required")
-        );
+        //println!("Value for uri: {:?}", matches.get_one::<String>("uri"));
 
         matches
+    }
+}
+
+pub mod ops {
+
+    use super::*;
+
+    pub async fn list(gcs_uri: &'static str, token: Arc<Token>) -> Result<()> {
+        let res = Client::new()
+            .get(gcs_uri)
+            .bearer_auth(token.as_str())
+            .send()
+            .await?;
+
+        println!("Status: {}", res.status());
+        println!("Headers:\n{:#?}", res.headers());
+
+        let body = res.text().await?;
+        println!("Body:\n{}", body);
+
+        Ok(())
+    }
+
+    pub async fn upload(token: Arc<Token>, file: String) -> Result<()> {
+        let gcs_uri: &'static str =
+            "https://storage.googleapis.com/upload/storage/v1/b/acrelab-production-us1c-transfer/o?uploadType=media&name=foo.txt";
+
+        let data_binary = std::fs::read(file).expect("File path doesn't exist");
+
+        let res = Client::new()
+            .post(gcs_uri)
+            .body(data_binary)
+            .bearer_auth(token.as_str())
+            .send()
+            .await?;
+
+        println!("Status: {}", res.status());
+        println!("Headers:\n{:#?}", res.headers());
+
+        let body = res.text().await?;
+        println!("Body:\n{}", body);
+
+        Ok(())
+    }
+
+    pub async fn get_resumable_upload_uri(
+        token: Arc<Token>,
+        file: String,
+    ) -> Result<header::HeaderValue, anyhow::Error> {
+        let gcs_uri: &'static str =
+            "https://storage.googleapis.com/upload/storage/v1/b/acrelab-production-us1c-transfer/o?uploadType=resumable&name=foo.txt";
+
+        let mut headers = header::HeaderMap::new();
+        headers.insert(
+            header::CONTENT_TYPE,
+            header::HeaderValue::from_static("application/json"),
+        );
+        headers.insert(
+            header::CONTENT_LENGTH,
+            header::HeaderValue::from_static("0"),
+        );
+
+        let res = Client::new()
+            .post(gcs_uri)
+            .headers(headers)
+            .bearer_auth(token.as_str())
+            .send()
+            .await?;
+
+        let uri = res
+            .headers()
+            .get("location")
+            .ok_or_else(|| anyhow::anyhow!("Missing 'location' in header"))?
+            .clone();
+
+        Ok(uri)
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     use cli::parse_args;
+    use ops::{get_resumable_upload_uri, list, upload};
 
+    // #TODO  CLI Arguments
     let matches = parse_args();
-    let uri = matches.get_one::<String>("uri").expect("required");
-    let op = matches.get_one::<String>("op").expect("required");
+    let uri = matches.get_one::<String>("uri");
+    let op = matches.get_one::<String>("op");
 
-    /// JSON API only supports a bearer token. I think this is only obtained through `gcloud
-    /// auth`, e.g.:
-    ///
-    /// curl -X GET \
-    ///   -H "Authorization: Bearer $(gcloud auth print-access-token)" \
-    ///   -H "x-goog-user-project: acrelab-production" \
-    ///   "https://storage.googleapis.com/storage/v1/b/acrelab-production-us1c-transfer/o?prefix=bourdain/xray/mortgage/one/cdl/json"
-    let gcs_uri: &'static str =
-        "https://storage.googleapis.com/storage/v1/b/acrelab-production-us1c-transfer/o/";
-
+    // Auth
     let provider = gcp_auth::provider().await?;
     let scopes = &["https://www.googleapis.com/auth/cloud-platform"];
     let token = provider.token(scopes).await?;
 
-    let res = reqwest::Client::new()
-        .get(gcs_uri)
-        .bearer_auth(token.as_str())
-        .send()
-        .await?;
+    // -- Print bearer token to stdout --
+    //println!("{}", token.as_str());
 
-    println!("Status: {}", res.status());
-    println!("Headers:\n{:#?}", res.headers());
+    // -- List bucket --
+    //list(Arc::clone(&token)).await?;
 
-    let body = res.text().await?;
-    println!("Body:\n{}", body);
+    // -- Upload object --
+    //upload(Arc::clone(&token), "./foo.txt".to_owned()).await?;
+
+    // -- Get URI for resumable uploads --
+    let uri = get_resumable_upload_uri(Arc::clone(&token), "./foo.txt".to_owned()).await?;
+    println!("{:?}", uri);
 
     Ok(())
 }
